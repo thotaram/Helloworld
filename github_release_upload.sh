d17fcc3021724512db3cffb3326a4f7490154678
Ram <ramanjineyulu67@gmail.com>
2022-11-03 19:19:12 +0530

    01.00.4 release

--- github_release_upload.sh
+++ github_release_upload.sh
@@ -5,17 +5,22 @@ GH_REPO_USER=thotaram
 GH_REPO=Helloworld
 GH_TARGET=main
 ASSETS_PATH=build
-VERSION='1.0.2'
+VERSION='01.00.4'
+ver=($(echo $VERSION | tr "." "\n"))
+MAJOR=${ver[0]}
+MINOR=${ver[1]}
+echo "Major: ${MAJOR}"
+echo "Minor: ${MINOR}"
 git add -u
 git commit -m "$VERSION release"
 git push
 res=`curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_PAT}" -X POST https://api.github.com/repos/${GH_REPO_USER}/${GH_REPO}/releases \
 -d "
 {
-  \"tag_name\": \"v$VERSION\",
+  \"tag_name\": \"v$MAJOR.$MINOR\",
   \"target_commitish\": \"$GH_TARGET\",
-  \"name\": \"v$VERSION\",
-  \"body\": \"Jenkins Release version $VERSION\",
+  \"name\": \"v$MAJOR.$MINOR\",
+  \"body\": \"Jenkins Release version $MAJOR.$MINOR\",
   \"draft\": false,
   \"prerelease\": false
 }"`
